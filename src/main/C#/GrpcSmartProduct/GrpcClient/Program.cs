3b1cdbcf9913dad0a3bbf5ec8e1f91b7ceb414d1
Brendan <x13342426@student.ncirl.ie>
2020-08-20 19:21:06 +0100

    Updating C# Service
    Changing the C# Service to display a stream of product information

--- src/main/C#/GrpcSmartProduct/GrpcClient/Program.cs
+++ src/main/C#/GrpcSmartProduct/GrpcClient/Program.cs
@@ -1,4 +1,5 @@
-﻿using Grpc.Net.Client;
+﻿using Grpc.Core;
+using Grpc.Net.Client;
 using GrpcServer;
 using System;
 using System.Threading.Tasks;
@@ -9,15 +10,42 @@ namespace GrpcClient
     {
         static async Task Main(string[] args)
         {
-            var input = new HelloRequest { Name = "Bren" };
+          //  var input = new HelloRequest { Name = "Bren" };
+          //  var channel = GrpcChannel.ForAddress("https://localhost:5001");
+          //  var client = new Greeter.GreeterClient(channel);
+
+
+          //  var reply = await client.SayHelloAsync(input);
+
+          //  Console.WriteLine(reply.Message);
+
             var channel = GrpcChannel.ForAddress("https://localhost:5001");
-            var client = new Greeter.GreeterClient(channel);
+            var productClient = new Product.ProductClient(channel);
+
+            var clientRequested = new ProductLookupModel { ProductId = 1 };
+
+            var product = await productClient.GetProductInfoAsync(clientRequested);
+
+            Console.WriteLine($"{ product.ProductName} { product.ProductType} { product.ProductDesc}");
+
+            using (var call = productClient.GetProductList(new ProductListRequest()))
+            {
+                while (await call.ResponseStream.MoveNext())
+                {
+                    var currentProduct = call.ResponseStream.Current;
+
+                    Console.WriteLine($" Product Name:{ currentProduct.ProductName} Product Type: { currentProduct.ProductType} Product Description: { currentProduct.ProductDesc} Price: { currentProduct.Price} ");
+
+                    Console.WriteLine();
+                    Console.WriteLine("New Product List");
+                    Console.WriteLine();
 
 
-            var reply = await client.SayHelloAsync(input);
+                }
+            }
 
-            Console.WriteLine(reply.Message);
-            Console.ReadLine();
+
+                Console.ReadLine();
         }
     }
 }
---
+++ src/main/C#/GrpcSmartProduct/GrpcServer/Services/ProductService.cs
@@ -0,0 +1,90 @@
+﻿using Grpc.Core;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GrpcServer.Services
+{
+    public class ProductService : Product.ProductBase
+    {
+
+        private readonly ILogger<ProductService> _logger;
+
+        public ProductService(ILogger<ProductService> logger)
+        {
+            _logger = logger;
+        }
+
+
+        public override Task<ProductModel> GetProductInfo(ProductLookupModel request, ServerCallContext context)
+        {
+            ProductModel output = new ProductModel();
+
+            if (request.ProductId == 1)
+            {
+                output.ProductName = "Pen";
+                output.ProductType = "Bic Brand";
+                output.ProductDesc = "Blue pen for writing";
+            }
+            else if (request.ProductId == 2)
+            {
+                output.ProductName = "Pen";
+                output.ProductType = "Bic Brand";
+                output.ProductDesc = "Blue pen for writing";
+            }
+            else
+            {
+                output.ProductName = "Pen";
+                output.ProductType = "Bic Brand";
+                output.ProductDesc = "Blue pen for writing";
+
+            }
+
+            return Task.FromResult(output);
+        }
+
+        public override async Task GetProductList(
+            ProductListRequest request,
+            IServerStreamWriter<ProductModel> responseStream,
+            ServerCallContext context)
+        {
+            List<ProductModel> products = new List<ProductModel>
+            {
+                new ProductModel
+                {
+
+                ProductName = "pen",
+                ProductType = "bic",
+                ProductDesc = "blue pen for writing",
+                Price = 5
+            },
+
+            new ProductModel
+            {
+                ProductName = "paper",
+                ProductType = "oxford",
+                ProductDesc = "a4 notepad for taking notes",
+                Price = 4
+            },
+             new ProductModel
+             {
+                ProductName = "eraser",
+                ProductType = "HB",
+                ProductDesc = "Eraser for removing pencil markings",
+                Price = 3
+            },
+
+        };
+
+            foreach (var prod in products)
+            {
+                await responseStream.WriteAsync(prod);
+            }
+        }
+
+
+
+    }
+}
