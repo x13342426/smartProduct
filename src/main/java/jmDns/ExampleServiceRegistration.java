afe5ed7d167c6b88c82298f147a1cc319f53b6d1
Brendan <x13342426@student.ncirl.ie>
2020-08-20 01:16:37 +0100

    Adding JmDNs

---
+++ src/main/java/jmDns/ExampleServiceRegistration.java
@@ -0,0 +1,37 @@
+
+package jmDns;
+
+import java.io.IOException;
+import java.net.InetAddress;
+
+import javax.jmdns.JmDNS;
+import javax.jmdns.ServiceInfo;
+
+// This code is adapted from https://github.com/jmdns/jmdns
+public class ExampleServiceRegistration {
+
+    public static void main(String[] args) throws InterruptedException {
+
+        try {
+            // Create a JmDNS instance
+            JmDNS jmdns = JmDNS.create(InetAddress.getLocalHost());
+
+            // Register a service
+            
+            ServiceInfo serviceInfo = ServiceInfo.create("_grpc._tcp.local.", "product_service", 9090, "test_Service_text");
+            jmdns.registerService(serviceInfo);
+            Thread.sleep(2000);
+            
+            
+            
+            
+            ServiceInfo serviceInfo1 = ServiceInfo.create("_http._tcp.local.", "print_product_service", 9091, "test_Service_text");
+            jmdns.registerService(serviceInfo1);
+            
+            
+
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+}
