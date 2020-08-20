71a345c14f454042dc08885472ac05b8959dbe0a
Brendan <x13342426@student.ncirl.ie>
2020-08-20 17:06:51 +0100

    Adding C# GRPC
    Setting up a new grpc server and client using C#

---
+++ src/main/C#/GrpcSmartProduct/GrpcClient/Program.cs
@@ -0,0 +1,23 @@
+﻿using Grpc.Net.Client;
+using GrpcServer;
+using System;
+using System.Threading.Tasks;
+
+namespace GrpcClient
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var input = new HelloRequest { Name = "Bren" };
+            var channel = GrpcChannel.ForAddress("https://localhost:5001");
+            var client = new Greeter.GreeterClient(channel);
+
+
+            var reply = await client.SayHelloAsync(input);
+
+            Console.WriteLine(reply.Message);
+            Console.ReadLine();
+        }
+    }
+}
