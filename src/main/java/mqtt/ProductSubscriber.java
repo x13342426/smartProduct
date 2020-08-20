edc2b88edf578845eaa9e95053246ee1451fe863
Brendan <x13342426@student.ncirl.ie>
2020-08-20 22:10:52 +0100

    Create ProductSubscriber.java
    Adding MQTT Service which will use the publish and subscribe method to receive messages.

---
+++ src/main/java/mqtt/ProductSubscriber.java
@@ -0,0 +1,36 @@
+package mqtt;
+
+import org.eclipse.paho.client.mqttv3.MqttClient;
+import org.eclipse.paho.client.mqttv3.MqttConnectOptions;
+import org.eclipse.paho.client.mqttv3.MqttException;
+import org.eclipse.paho.client.mqttv3.persist.MemoryPersistence;
+
+public class ProductSubscriber {
+
+    public static void main(String[] args) {
+
+        String broker = "tcp://localhost:1883";
+        String clientId = "Subscriber";
+        MemoryPersistence persistence = new MemoryPersistence();
+
+        try {
+            MqttClient sampleClient = new MqttClient(broker, clientId, persistence);
+            MqttConnectOptions connOpts = new MqttConnectOptions();
+            connOpts.setCleanSession(true);
+            sampleClient.setCallback(new Subscriber());
+            
+            System.out.println("Connecting to broker: " + broker);
+            sampleClient.connect(connOpts);
+            System.out.println("Connected");
+            sampleClient.subscribe("/product/#");
+            
+        } catch (MqttException me) {
+            System.out.println("reason " + me.getReasonCode());
+            System.out.println("msg " + me.getMessage());
+            System.out.println("loc " + me.getLocalizedMessage());
+            System.out.println("cause " + me.getCause());
+            System.out.println("excep " + me);
+            me.printStackTrace();
+        }
+    }
+}
